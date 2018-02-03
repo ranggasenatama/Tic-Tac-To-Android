89523f915ffdb0b28d243e161eb513e91b6e34ef
Rangga <ranggasenatama@gmail.com>
2018-02-04 03:57:49 +0700

    Drop In Method

--- app/src/main/java/com/example/asus/tictacto/MainActivity.java
+++ app/src/main/java/com/example/asus/tictacto/MainActivity.java
@@ -2,8 +2,29 @@ package com.example.asus.tictacto;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    boolean redOrYellow = false;
+
+    public void dropIn(View view)
+    {
+        ImageView counter = (ImageView) view;
+
+        if(redOrYellow)
+        {
+            counter.setImageResource(R.drawable.red);
+            redOrYellow =false;
+        }
+        else
+        {
+            counter.setImageResource(R.drawable.yellow);
+            redOrYellow = true;
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
