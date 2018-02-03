6f628fdaa7bc75595afc998264861c46290e3200
Rangga <ranggasenatama@gmail.com>
2018-02-04 04:52:32 +0700

    winning game

--- app/src/main/java/com/example/asus/tictacto/MainActivity.java
+++ app/src/main/java/com/example/asus/tictacto/MainActivity.java
@@ -4,9 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
-
-import java.util.HashMap;
-import java.util.Map;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,9 +12,9 @@ public class MainActivity extends AppCompatActivity {
 
     int isTerisi[] = {2,2,2,2,2,2,2,2,2};
 
-    boolean redOrYellow = false;
+    int winningGame[][] = {{0,1,2}, {3,4,5}, {6,7,8}, {0,3,6}, {1,4,7}, {2,5,8}, {0,4,8}, {2,4,8}};
 
-    Map enterIn = new HashMap();
+    boolean redOrYellow = false;
 
     public void dropIn(View view)
     {
@@ -34,6 +32,14 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
+        for(int i=0;i<8;i++){
+            if(isTerisi[winningGame[i][0]]==1 && isTerisi[winningGame[i][1]]==1 && isTerisi[winningGame[i][2]]==1){
+                Toast.makeText(this, "Win Red", Toast.LENGTH_SHORT).show();
+            }
+            if(isTerisi[winningGame[i][0]]==0 && isTerisi[winningGame[i][1]]==0 && isTerisi[winningGame[i][2]]==0){
+                Toast.makeText(this, "Win Yellow", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
     @Override
