97e09c2cee9c8c79d5c2f990df626ebb8f55865f
Rangga <ranggasenatama@gmail.com>
2018-02-04 04:37:24 +0700

    fix bug double tap

--- app/src/main/java/com/example/asus/tictacto/MainActivity.java
+++ app/src/main/java/com/example/asus/tictacto/MainActivity.java
@@ -5,23 +5,33 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
 
+    //0: Yellow, 1: Red
+
+    int isTerisi[] = {2,2,2,2,2,2,2,2,2};
+
     boolean redOrYellow = false;
+
+    Map enterIn = new HashMap();
 
     public void dropIn(View view)
     {
         ImageView counter = (ImageView) view;
 
-        if(redOrYellow)
-        {
-            counter.setImageResource(R.drawable.red);
-            redOrYellow =false;
-        }
-        else
-        {
-            counter.setImageResource(R.drawable.yellow);
-            redOrYellow = true;
+        if(isTerisi[Integer.parseInt(counter.getTag().toString())] == 2) {
+            if (redOrYellow) {
+                counter.setImageResource(R.drawable.red);
+                redOrYellow = false;
+                isTerisi[Integer.parseInt(counter.getTag().toString())]=1;
+            } else {
+                counter.setImageResource(R.drawable.yellow);
+                redOrYellow = true;
+                isTerisi[Integer.parseInt(counter.getTag().toString())]=0;
+            }
         }
 
     }
